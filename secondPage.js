8783c1deda74b3aea1ec5f78b049c41ea5e102fc
yahiakd-crypto <kadayahia20@gmail.com>
2025-04-15 16:06:21 +0100

    Add files via upload

---
+++ secondPage.js
@@ -0,0 +1,8 @@
+// ...existing code...
+
+document.getElementById('themeToggleButton').addEventListener('click', () => {
+    document.body.style.backgroundColor = 'black';
+    document.body.style.color = 'white';
+});
+
+// ...existing code...
